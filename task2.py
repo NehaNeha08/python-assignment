7933725958172b1c09e89b46da65c728558e5319
Neha.. <jaralneha8@gmail.com>
2025-08-06 22:09:14 +0530

    Add files via upload
    Added task2.py for personalized greetings

---
+++ task2.py
@@ -0,0 +1,7 @@
+# Task 2: Personalized Greeting
+
+# Take user's name as input
+name = input("Enter your name: ")
+
+# Print a personalized greeting message
+print("Hello " + name + "! Have a great day!")
